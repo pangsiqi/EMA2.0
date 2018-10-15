7cf0436392a152a88fdea502325b5b75dbd424f6
pangsiqi <746609995@qq.com>
2018-10-15 14:09:47 +0800

    first

---
+++ static/js/login.js
@@ -0,0 +1,31 @@
+$(function(){
+	var userinfo = new Array();
+	userinfo[0] = {
+		name:"fuyg",
+		pass:"fuyg"
+	};
+	userinfo[1] = {
+		name:"jidd",
+		pass:"jidd"
+	};
+
+	$(".btn").on("click",function(){
+		var userName = $("#username").val();
+		var userPass = $("#password").val();
+		console.log(userName);
+		console.log(userPass);
+		for(var i = 0;i<userinfo.length;i++){
+			if(userName == userinfo[i].name && userPass == userinfo[i].pass){
+				alert("登录成功");
+				window.location.href="./index.html";
+				return;
+			}else{
+				alert("用户名或密码错误");
+				return;
+			}
+		}
+	});
+	
+		
+			
+});
